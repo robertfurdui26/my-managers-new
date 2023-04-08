17708332c4eb1b87eeca690ef72868eb20a17128
robertfurdui26 <125736128+robertfurdui26@users.noreply.github.com>
2023-04-08 18:29:32 +0300

    new commit

--- src/App.js
+++ src/App.js
@@ -3,15 +3,19 @@ import "./index.css";
 import Navbar from "./components/Navbar";
 import Events from "./components/Events";
 import Profile from "./components/Profile";
+import Salary from "./components/Salary";
 import cards from "./data";
 
 function App() {
-  const cardsNew = cards.map((item) => {});
+  const profileNew = cards.map((item) => {
+    return <Profile key={item.id} item={item} />;
+  });
   return (
     <div className="App">
       <Navbar />
       <Events />
-      <Profile />
+      <div className="profile-section">{profileNew}</div>
+      <Salary />
     </div>
   );
 }
--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,19 +1,18 @@
 import React from "react";
 import round from "../images/rrrrr.jpg";
 
-export default function Profile() {
+export default function Profile(props) {
   return (
     <div>
       <div class="card-container">
         <span class="pro">PRO</span>
         <img class="round" src={round} alt="user" />
-        <div className="props">
-          <h3>Ricky Park</h3>
-          <h6> Location: America</h6>
-          <p>City:New York </p>
-          <p>Salary :$5000 /month</p>
-          <p>Job: Engineer</p>
-          <p>Email: ricky_park@yahoo.com</p>
+        <div className="property">
+          <h3>{props.item.name}</h3>
+          <h6> {props.item.location}</h6>
+          <p className="props-size">{props.item.city} </p>
+          <p className="props-size">{props.item.job}</p>
+          <p className="props-size">{props.item.email}</p>
         </div>
       </div>
     </div>
